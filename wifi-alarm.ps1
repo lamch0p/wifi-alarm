21b6e88762cffb5f346cb35646707d7bbcdd2817
Devin Lam <devin@codelam.com>
2024-01-09 17:32:43 -0500

    Added parameters for specifying ssid, poll seconds, outage threshold, audio filename, start and end time range, and conditions for outputting log files and verbosity.

--- wifi-alarm.ps1
+++ wifi-alarm.ps1
@@ -1,20 +1,112 @@
-$SSID = "WifiSSID"  # Replace with your WiFi network name
-$AudioFile = ".\alarm.wav"  # Replace with your sound file path
-$ConnectionLost = $false
-$AudioFile =  (New-Object Media.SoundPlayer $AudioFile)
-Write-Host "Monitoring for connection to SSID: $SSID"
+param(
+    [string] $ssid,
+    [int] $pollSeconds = 5,
+    [int] $outageThresholdSeconds = 15,
+    [string] $audioFilename,
+    [string] $startTime,
+    [string] $endTime,
+    [switch] $outputLog = $false,
+    [switch] $verbose = $false
+)
+
+function DoAlarm {
+    if ($outageDuration -ge $outageThresholdSeconds ) {
+        $currentTime = Get-Date -Format "HH:mm"
+        # Check if the current time falls within the specified range
+        if (($currentTime -ge $startTime -and $currentTime -le $endTime) -or ($startTime -eq "" -or $endTime -eq "")) {
+            $audioFile.Play()
+        }
+        else {
+            $audioFile.Stop()
+        }
+    }   
+}
+
+function WriteHost {
+    param(
+        [string] $message,
+        [Alias('m')] $messageAlias
+    )
+    if ($verbose) {
+        Write-Host $messageAlias
+    }
+}
+
+if (!$ssid) {
+    Write-Error "No SSID provided. Exiting."
+    exit 1
+}
+
+function ConfigureLogging {
+    $date = Get-Date -Format "yyyy-MM-dd"
+    $csvPath = ".\logs\$date-wifi-alarm-outage-log.csv"
+    if (!(Test-Path $csvPath)) {
+        if ($verbose) {
+            New-Item $csvPath -ItemType File -Force
+        }
+        else {
+            New-Item $csvPath -ItemType File -Force | Out-Null
+        }
+        $headers = [PSCustomObject]@{
+            SSID     = $null
+            Detected = $null
+            Restored = $null
+            Duration = $null
+        }
+        $headers | Export-Csv $csvPath -NoTypeInformation
+    }
+    return $csvPath
+}
+
+# Check if file exists
+$filePath = [System.IO.Path]::Combine(".\res\audio", $audioFilename)
+
+if ([System.IO.File]::Exists($filePath)) {
+    WriteHost -m "Using audio file '$filePath'"
+    $audioFile = (New-Object Media.SoundPlayer $filePath)
+}
+else {
+    WriteHost -m "The audio file at '$filePath' does not exist. Using default audio file."
+    $audioFile = (New-Object Media.SoundPlayer '.\res\audio\alarm.wav')
+}
+
+$wifiDisconnected = $false
+$outageDuration = 0
+
+Write-Host "Monitoring connection to SSID: $ssid"
+
+$outageEvent = [PSCustomObject]@{ SSID = $null; Detected = $null; Restored = $null; Duration = $null }
 
 while ($true) {
-    $ConnectedSSID = (Get-NetConnectionProfile).Name  
-    if ($ConnectedSSID -ne $SSID) {
-        $AudioFile.Play()
-        $ConnectionLost = $true
-        Write-Host "WiFi connection lost!"
+    if ($outputLog) {
+        $csvPath = ConfigureLogging
     }
-    elseif ($ConnectionLost) {
-        Write-Host "WiFi connection restored."
-        $AudioFile.Stop()
-        $ConnectionLost = $false
+    $connectedSSID = (Get-NetConnectionProfile).Name 
+    $outageEvent.SSID = $connectedSSID
+    if ($wifiDisconnected) {
+        $outageDuration += $pollSeconds
     }
-    Start-Sleep -Seconds 5  # Check every 5 seconds (adjust as needed)
+    if ($connectedSSID -ne $ssid) {
+        WriteHost -m "WiFi disconnected."
+        WriteHost -m "Outage duration $outageDuration seconds"
+        $wifiDisconnected = $true 
+        if (!$outageEvent.Detected) {
+            $outageEvent.Detected = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        }
+        DoAlarm
+    }
+    elseif ($wifiDisconnected) {
+        WriteHost -m "WiFi connection restored."
+        $audioFile.Stop()
+        $wifiDisconnected = $false
+        $outageEvent.Restored = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $outageEvent.Duration = $outageDuration
+        if ($outputLog) {
+            $outageEvent | Export-Csv $csvPath -NoTypeInformation -Append
+        }
+        $outageEvent = [PSCustomObject]@{ SSID = $null; Detected = $null; Restored = $null; Duration = $null }
+        $outageDuration = 0
+    }
+    Start-Sleep -Seconds $pollSeconds  # Check every 5 seconds (adjust as needed)
 }
+
